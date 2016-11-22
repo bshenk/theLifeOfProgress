e261e57e3a8b8148250c692109f54f73aadf3135
bshenkd <bshenkd@gmail.com>
2016-11-22 10:19:35 -0500

    continued work on the routine component

--- src/components/routine/index.vue
+++ src/components/routine/index.vue
@@ -12,8 +12,24 @@
 				</div>
 				<div class="tasks">
 					<ul>
-						<li v-for="task in taskblock.tasks">
-							<input type="checkbox" />{{ task }}
+						<li v-for="task in taskblock.tasks" class="task">
+							<label>
+								<input type="checkbox" :checked="task.completed"/>
+								<span></span>
+								<div class="description">
+									<div :class="[{ completed: task.completed }, 'title']">{{ task.name }}</div>
+									<div class="modifiers">
+										<template v-for="modifier in task.modifiers">
+											<div
+												class="icon"
+												:class="[{ enabled: modifier.enabled }, `icon-${modifier.icon}`]"
+												@click="modifierToggle(modifier.name)"
+											>
+											</div>
+										</template>
+									</div>
+								</div>
+							</label>
 						</li>
 					</ul>
 				</div>
